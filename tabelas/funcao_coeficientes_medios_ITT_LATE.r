2bab246e7b0ae946096aae6eafe8ef6fb7c06496
gabrielcaser <gabrielcaser9@hotmail.com>
2024-03-25 10:08:39 -0300

    sem arredondar
    Retirei os arredondamentos dos coeficientes.

--- tabelas/funcao_coeficientes_medios_ITT_LATE.r
+++ tabelas/funcao_coeficientes_medios_ITT_LATE.r
@@ -9,21 +9,19 @@ if(!require(estimatr)) install.packages("estimatr")    # testes late
 if(!require(ivreg)) install.packages("ivreg")       # testes late
 if(!require(sandwich)) install.packages("sandwich")    # testes late
 
-  total_regs <- 0 # Inicializando o contador de gráficos - para dizer no final quantas vc criou :)
-  
+total_regs <- 0 # Inicializando o contador de gráficos - para dizer no final quantas vc criou :)
+
 # Função para estimar e salvar resultados
 f_oppen_estima_ITT_LATE     <- function(dados,
-										                    vars_controle,
-										                    vars_resultado,
-										                    var_tratamento_sorteado, # tratamento atribuído pelo sorteio (para calcular o ITT)
-										                    var_tratamento_recebido, # tratamento de fato (para calcular o LATE)
-										                    tempo_final = NULL,       # último tempo do impacto
-										                    output_path
+                                        vars_controle,
+                                        vars_resultado,
+                                        var_tratamento_sorteado, # tratamento atribuído pelo sorteio (para calcular o ITT)
+                                        var_tratamento_recebido, # tratamento de fato (para calcular o LATE)
+                                        tempo_final = NULL,      # último tempo do impacto
+                                        output_path
 ) {
   
   dados_final <- data.frame() # dataframe vazio para armazenar resultados do loop
-  
-  
   
   
   for (i in seq_along(vars_resultado)) {
@@ -32,17 +30,17 @@ f_oppen_estima_ITT_LATE     <- function(dados,
       
       for(w in seq_along(var_tratamento_recebido)) {
         
-        var <- vars_resultado[i]
-        
+        # Definindo parâmetros do loop
+        var            <- vars_resultado[i]
         var_trat_receb <- var_tratamento_recebido[w]
-        
-        total_regs <- total_regs + 1
-        
         dados$tratamento_sorteado <- dados[[var_tratamento_sorteado]] # criando variável
         dados$tratamento_recebido <- dados[[var_trat_receb]] # criando variável
         
+        total_regs <- total_regs + 1
+        
+        
         # Estimando ITT com variáveis e com erro padrão simples
-        model_ITT <- lm(dados[[var]][tempo == t] ~ dados$tratamento_sorteado[tempo == t], data = dados)
+        model_ITT <- lm(dados[[var]][tempo == t] ~ dados$tratamento_sorteado[tempo == t] , data = dados)
         
         formula_ITT_control <- formula(paste0(var, "[tempo == ", t, "] ~ tratamento_sorteado[tempo == ", t, "] + ", vars_controle))
         model_ITT_control <- lm(formula_ITT_control, data = dados)
@@ -64,16 +62,18 @@ f_oppen_estima_ITT_LATE     <- function(dados,
         # Adicionando resultados em um dataframe
         dados_resultados <- data.frame(
           variavel = c(var, var, var, var),
-          efeito = c(round(model_ITT$coefficients[[2]], 2), round(model_LATE$coefficients[[2]], 2), round(model_ITT_control$coefficients[[2]], 2), round(model_LATE_control$coefficients[[2]], 2)),
-          erro_padrao = c(round(model_ITT_rob[nrow(model_ITT_rob) + 2], 2), round(model_LATE_rob[nrow(model_LATE_rob) + 2], 2), round(model_ITT_control_rob[nrow(model_ITT_control_rob) + 2], 2), round(model_LATE_control_rob[nrow(model_LATE_control_rob) + 2], 2)),
+          efeito = c(model_ITT$coefficients[[2]], model_LATE$coefficients[[2]], model_ITT_control$coefficients[[2]], model_LATE_control$coefficients[[2]]),
+          erro_padrao = c(model_ITT_rob[nrow(model_ITT_rob) + 2], model_LATE_rob[nrow(model_LATE_rob) + 2], model_ITT_control_rob[nrow(model_ITT_control_rob) + 2], model_LATE_control_rob[nrow(model_LATE_control_rob) + 2]),
           n_obs = c(nrow(model_ITT[["model"]]), nrow(model_LATE[["model"]]), nrow(model_ITT_control[["model"]]),  nrow(model_LATE_control[["model"]])),
-          p_val = c(round(model_ITT_rob[nrow(model_ITT_rob) * 3 + 2], 2), round(model_LATE_rob[nrow(model_LATE_rob) * 3 + 2], 2), round(model_ITT_control_rob[nrow(model_ITT_control_rob) * 3 + 2], 2), round(model_LATE_control_rob[nrow(model_LATE_control_rob) * 3 + 2], 2)),
+          p_val = c(model_ITT_rob[nrow(model_ITT_rob) * 3 + 2], model_LATE_rob[nrow(model_LATE_rob) * 3 + 2], model_ITT_control_rob[nrow(model_ITT_control_rob) * 3 + 2], model_LATE_control_rob[nrow(model_LATE_control_rob) * 3 + 2]),
           estimador = c("ITT", "LATE", "ITT", "LATE"),
           controles  = c("não", "não", "sim", "sim"),
           tempo =  t
         )
         
-        dados_resultados <- dados_resultados %>% mutate(tratamento_completo = var_trat_receb)
+        dados_resultados <- dados_resultados %>%
+          mutate(tratamento_completo = var_trat_receb)
+        
         dados_final <- bind_rows(dados_final, dados_resultados) # juntando dataframes
         
         # Tendando calcular intervalos de confiança
@@ -90,6 +90,7 @@ f_oppen_estima_ITT_LATE     <- function(dados,
       }
     }
   }
+  
   
   # Salvando
   write_xlsx(dados_final, path = paste0(output_path, "/tabelas/coef_medios_ITT_LATE.xlsx"))
